4d94d4b5387eac0035e1aabf47daf8050c81ba48
mingwei.zhang <mingwei.zhang@mybogames.com>
2021-01-08 17:19:04 +0800

    Vue单文件组件模版

---
+++ Advanced/使用Vue单文件组件/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+	
+	<!-- 使用自定义组件HelloWorld -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	
+  </div>
+</template>
+
+<script>
+	
+// 导入HelloWorld.vue文件并命名为HelloWorld变量
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld	//注册HelloWorld变量为组件
+  },
+  data() {
+	  return {
+		  title: 'Hello'
+	  }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Advanced/使用Vue单文件组件/src/components/MyComponent.vue
@@ -0,0 +1,42 @@
+<template>
+	<!-- 组件模版内容 -->
+	<div>
+		<slot name="begin" v-bind:cValue="cValue" :user="user" >
+			默认内容
+		</slot>
+		
+		<button>
+			子组件
+			<slot>center默认内容</slot>
+		</button>
+		<slot name="end">end默认内容</slot>
+	</div>
+</template>
+
+<script>
+	export default {
+		// 组件名
+		data () {
+			return {
+				cValue: "内部内容",
+				user: {
+					firstName: 'wang',
+					lastName: 'zhirui'
+				}
+			}
+		},
+		// 对外开发的属性
+		props:[],
+		//组件生命周期函数
+		created() {
+			
+		},
+		// 组件内部方法
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+</style>
