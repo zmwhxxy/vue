dd090547a44a25835aab7067cf88ef2e3ef8ec23
eli <2556246863@qq.com>
2024-04-01 10:51:45 +0800

    整理目录结构

---
+++ vue3_projects/hello_vue3/src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+<!-- 组件结构 html -->
+<div class="app">
+    <h1>你好啊</h1>
+</div>
+
+</template>
+
+
+<script lang="ts">
+// 组件脚本：js 或者 ts
+    export default {
+        name:'App' //组件名：App
+    }
+</script>
+
+
+<style>
+/* 组件样式：层叠样式表 */
+.app {
+    background-color: aqua;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
