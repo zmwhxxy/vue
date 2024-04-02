dc147df0d3a88128a8edc0e661f50d694b91ea03
eli <2556246863@qq.com>
2024-04-02 11:10:12 +0800

    提交目录

---
+++ vue3_projects/02_components/src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+<!-- 组件结构 html -->
+<Person/>
+
+</template>
+
+
+<script lang="ts">
+// 组件脚本：js 或者 ts
+    import Person from "./components/Person.vue";
+
+    export default {
+        name:'App', //组件名：App
+        components: {   //注册组件
+            Person
+        }
+    }
+</script>
+
+
+<style>
+/* 组件样式：层叠样式表 */
+
+</style>
---
+++ vue3_projects/02_components/src/components/Person.vue
@@ -0,0 +1,49 @@
+
+<template>
+    <div>
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click = "changeName">修改名字</button>
+        <button @click = "changeAge">修改年龄</button>
+        <button @click = "showTel">查看联系方式</button>
+        <hr>
+        
+    </div>
+</template>
+
+
+<script lang="ts">
+    export default {
+        name: 'Person'
+    }
+</script>
+
+<script lang="ts" setup>
+    //ref 响应式的数据用ref函数
+    import {ref} from 'vue'
+
+   let name = ref("张三");
+   let age = ref(18);
+   let tel = 13888888888;
+
+    function changeName() {
+        name.value = "李四";
+        console.log(name.value); 
+    }
+
+    function changeAge() {
+        age.value += 1;
+        console.log(age.value);
+    }
+
+    function showTel() {
+        alert("点击这里查看电话号码");
+    }
+</script>
+
+
+<style>
+    button {
+        margin-right: 10px;
+    }
+</style>
